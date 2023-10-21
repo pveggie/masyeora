1f766ee24a2772b6fed8363245198ae52c76a65e
Gemma Gotch <pveggie@hotmail.com>
2023-10-21 14:38:59 +0200

    feat([WIP] play game): can close modal to reset game

--- components/NunchiGame.vue
+++ components/NunchiGame.vue
@@ -11,6 +11,10 @@ const updateCurrentCardValue = (value: number) => {
 const isGameOver = computed(() => {
   return maxCardValue.value <= currentCardValue.value
 })
+
+const resetGame = () => {
+  currentCardValue.value = 1
+}
 </script>
 
 <template>
@@ -21,8 +25,16 @@ const isGameOver = computed(() => {
       @update-current-card-value="updateCurrentCardValue"
     ></nunchi-table>
 
-    <v-dialog v-model="isGameOver">
-      <v-card> Game finished. Reset? </v-card>
+    <v-dialog v-model="isGameOver" width="auto">
+      <v-card width="50vw" height="50vh" >
+        <v-card-text>
+          Game finished.
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" block @click="resetGame">Reset</v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card>  </v-card>
     </v-dialog>
   </div>
 </template>
